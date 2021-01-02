302be5f57c575f92292b323b1d208aed831b2fc1
Joseph <wjsdudtkd95@gmail.com>
2021-01-02 21:13:06 +0900

    [feat] MovieDetail Ui 추가

---
+++ app/src/main/java/com/joseph/mvvmbasic/ui/MovieDetailActivity.kt
@@ -0,0 +1,12 @@
+package com.joseph.mvvmbasic.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.joseph.mvvmbasic.R
+
+class MovieDetailActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_movie_detail)
+    }
+}
